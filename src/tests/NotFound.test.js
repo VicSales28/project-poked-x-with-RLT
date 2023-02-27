85b7dcab3fb16b652eea6e86711617b0d2341bf1
Victoria Sales <contatovictoriasales@gmail.com>
2023-02-27 19:27:56 -0300

    Testa o componente <NotFound.js /> (Req. 04)

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente <NotFound.js />', () => {
+  beforeEach(() => { renderWithRouter(<NotFound />); });
+  test('Testa se a página contém um heading h2 com o texto Page requested not found', () => {
+    const textNotFound = screen.getByRole('heading', {
+      name: 'Page requested not found',
+      level: 2,
+    });
+    expect(textNotFound).toBeVisible();
+  });
+
+  test('Testa se a página mostra a imagem escolhida', () => {
+    const image = screen.getByRole('img');
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image).toBeVisible();
+    expect(image).toHaveAttribute('src', url);
+  });
+});
